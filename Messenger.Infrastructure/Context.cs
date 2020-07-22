c9d32e46e648f9a8c7333d20e2006d1a50a74b86
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2020-07-22 13:50:08 +0200

    Initialize

---
+++ Messenger.Infrastructure/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messenger.Data
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Messenger.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Messenger.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+
+
+            return services;
+        }
+    }
+}
