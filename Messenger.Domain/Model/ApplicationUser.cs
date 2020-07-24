5a85e5e53255a48874dae829574d42c68afe9f12
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2020-07-24 13:27:39 +0200

    Added Message model | Added Friends and Messages to ApplicationUser Model

--- Messenger.Domain/Model/ApplicationUser.cs
+++ Messenger.Domain/Model/ApplicationUser.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace Messenger.Domain.Model
 {
     public class ApplicationUser : IdentityUser
     {
+        public List<ApplicationUser> Friends { get; set; }
+        public List<Message> Messages { get; set; }
     }
 }
---
+++ Messenger.Domain/Model/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Messenger.Domain.Model
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Time { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser SentTo { get; set; }
+    }
+}
