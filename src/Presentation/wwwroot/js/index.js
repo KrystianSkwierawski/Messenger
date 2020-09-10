ede77eb3ba5bf35156ec78274c4422132f6f59df
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2020-09-10 18:38:40 +0200

    Added settings section

--- src/Presentation/wwwroot/js/index.js
+++ src/Presentation/wwwroot/js/index.js
@@ -4,3 +4,7 @@ import * as indexView from './views/taskView.js';
 elements.menuButton.addEventListener('click', () => {
     indexView.slideOutSideMenu();
 });
+
+elements.addFriendButton.addEventListener('click', () => {
+    const friendName = prompt('Friend name:');
+});
--- src/Presentation/wwwroot/js/views/base.js
+++ src/Presentation/wwwroot/js/views/base.js
@@ -1,7 +1,8 @@
 ﻿export const elements = {
     menuButton: document.querySelector('.menu__button'),
     menuContainer: document.querySelector('.side-menu__container'),
-    menuFriendName: document.querySelector('.menu__friend-name')
+    menuFriendName: document.querySelector('.menu__friend-name'),
+    addFriendButton: document.querySelector('.add-friend__button')
 };
 
 export const elementStrings = {
