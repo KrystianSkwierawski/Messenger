e92263a45304dee3d36e2c7a8972888e85cd40f6
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2020-10-07 13:25:31 +0200

    Sorted and removed usings GetFriendsByUserIdAndRelationShipsQuery

--- src/Application/Friends/Query/GetFriendsByUserIdAndRelationShipsQuery.cs
+++ src/Application/Friends/Query/GetFriendsByUserIdAndRelationShipsQuery.cs
@@ -1,9 +1,7 @@
 ﻿using Domain.Model;
 using MediatR;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
