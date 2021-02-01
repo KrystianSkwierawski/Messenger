3033d47c0d701e11a27237359248d50fe17360ee
SZEFNUDY <52860350+SZEFNUDY@users.noreply.github.com>
2021-02-01 16:58:12 +0100

    no message

---
+++ tests/Messenger.Application.IntegrationTests/ApplicationUsers/Queries/GetThemeByUserIdTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.ApplicationUsers.Queries;
+using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Messenger.Application.IntegrationTests.ApplicationUsers.Queries
+{
+    using static Testing;
+    public class GetThemeByUserIdTests : TestBase
+    {
+        [Test]
+        public async Task ShouldReturnUserTheme()
+        {
+            // Arrange
+            ApplicationUser user = await CreateUserAsync("User1", "Testing1234!", new string[] { });
+
+
+            // Act
+            var query = new GetThemeByUserIdQuery
+            {
+                UserId = user.Id
+            };
+
+            var result = await SendAsync(query);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().Be(user.Theme);
+        }
+    }
+}
